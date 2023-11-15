cde139c089aab43028be5058b7f58774f645c839
Edoardo Cocciò <edoardo.coccio@outlook.it>
2023-11-15 11:46:14 +0100

    Added the database exploration file

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import mysql.connector as mysql
 from mysql.connector import Error
 
-# MySQL DB credentials
-user = "root"
-password = "password"
-
-
-def createdb(user:str, passw:str):
-    db = mysql.connect(host="localhost", user=user, passwd=passw)
+def createdatabase(user:str,passw:str):
+    db = mysql.connect(
+        host="localhost", 
+        user=user, 
+        passwd=passw)
     curs = db.cursor()
-
-    databasecreation = "CREATE DATABASE Chess"
-
-    curs.execute(databasecreation)
-
+    curs.execute("CREATE DATABASE Jobs")
 
 def creattables(user:str,passw:str):
     db = mysql.connect(
         host="localhost", 
         user=user, 
         passwd=passw, 
-        database="chess")
+        database="Jobs")
     
     curs = db.cursor()
 
-    company = "CREATE TABLE Location(" \
-                       "longitude FLOAT(6,4) NOT NULL" \
-                       "latitude FLOAT(6,4) NOT NULL" \
-                       "location VARCHAR(100) NOT NULL," \
-                       "country VARCHAR(100) NOT NULL" \
-                       "PRIMARY KEY (longitude, latitude)" \
-                       ");"
+    location = "CREATE TABLE Location(" \
+                    "longitude FLOAT(6,4) NOT NULL" \
+                    "latitude FLOAT(6,4) NOT NULL" \
+                    "location VARCHAR(100) NOT NULL," \
+                    "country VARCHAR(100) NOT NULL" \
+                    "PRIMARY KEY (longitude, latitude)" \
+                    ");"
     
-    curs.execute(company)
+    role = "CREATE TABLE Role("\
+                    "responsibilities VARCHAR(250) NOT NULL" \
+                    "skills VARCHAR(300) NOT NULL" \
+                    "job_description VARCHAR(430) NOT NULL" \
+                    "job_title VARCHAR(35) NOT NULL" \
+                    "role VARCHAR(40) NOT NULL PRIMARY KEY" \
+                    ");"
 
+    company = "CREATE TABLE Company("\
+                    "company VARCHAR(100) NOT NULL PRIMARY KEY" \
+                    
+    
+    curs.execute(location)
+    curs.execute(role)
+    curs.execute(company)
+    
