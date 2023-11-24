07cf2dce9cf5ce0bbb3f41eabf2bb2ed3588394d
Edoardo Cocciò <edoardo.coccio@outlook.it>
2023-11-24 13:25:29 +0100

    Added a complete skeleton on how to load and create each table

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import mysql.connector as mysql
 from mysql.connector import Error
 import pandas as pd
-
-user = '...'
-password = '...'
+import getpass
 
 def createdatabase(user:str,passw:str):
     db = mysql.connect(
         host="localhost", 
         user=user, 
         passwd=passw)
-    curs = db.cursor()
-    curs.execute("CREATE DATABASE Jobs")
+    if db.is_connected():
+        curs = db.cursor()
+        curs.execute('SHOW DATABASES')
+        result = curs.fetchall()
+
+        for x in result:
+            if 'Jobs' == x[0]:
+                curs.execute(f'DROP DATABASE Jobs')
+                db.commit()
+                print('The database already exists. The old one was dropped.')
+
+        curs.execute("CREATE DATABASE Jobs")
+        print('The database was created!')
 
 def creattables(user:str,passw:str):
     db = mysql.connect(
@@ -64,8 +73,6 @@ def creattables(user:str,passw:str):
                     "company_size MEDIUMINT" \
                     ");"
     
-
-    
     curs.execute(location)
     curs.execute(role)
     curs.execute(company)
@@ -83,20 +90,57 @@ def dataload(user:str, password:str):
     
     curs = db.cursor()
     
-
-    for _, row in data.iterrows():
+    # Add data to the Company table
+    for _, row in data[['Company', 'City', 'State', 'Industry', 'Sector', 'Zip', 'CEO', 'Website', 'Ticker']].iterrows():
         try:
             curs.execute("""
                 INSERT INTO Company (company, city, state, industry, sector, zip, ceo, website, ticker)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
             """, (row['Company'], row['City'], row['State'], row['Industry'], row['Sector'], row['Zip'], row['CEO'], row['Website'], row['Ticker']))
             db.commit()
-        except Error as err:
+        except Error as e:
             db.rollback()
-            print(f"Error: '{err}'")
+            print(f"Error: '{e}'")
+    
+    # Add data to the Location table
+    for _, row in data[['longitude', 'latitude', 'location', 'Country']].iterrows():
+        try:
+            curs.execute("""
+                INSERT INTO Location (longitude, latitude, location, country)
+                VALUES (%s, %s, %s, %s);
+            """, (row['longitude'], row['latitude'], row['location'], row['Country']))
+            db.commit()
+        except Error as e:
+            db.rollback()
+            print(f"Error: '{e}'")
+
+    # Add data to the Role table
+    for _, row in data[['Role', 'Job Title', 'Job Description', 'skills', 'Responsibilities']].iterrows():
+        try:
+            curs.execute("""
+                INSERT INTO Role (longitude, latitude, location, country)
+                VALUES (%s, %s, %s, %s);
+            """, (row['longitude'], row['latitude'], row['location'], row['Country']))
+            db.commit()
+        except Error as e:
+            db.rollback()
+            print(f"Error: '{e}'")
+
+    # Add data to the Offer table
+    for _, row in data[['Job Id', 'Work Type', 'Qualifications', 'Preference', 'Benefits', 'Experience', 'Salary Range', 'Contact', 'Contact Person', 'Company Size']].iterrows():
+        try:
+            curs.execute("""
+                INSERT INTO Offer (job_id, work_type, qualifications, preference, benefits, experience, salary_range, contact, contact_person, company_size)
+                VALUES (%s, %s, %s, %s);
+            """, (row['Job Id'], row['Work Type'], row['Qualifications'], row['Preference'], row['Benefits'], row['Experience'], row['Salary Range'], row['Contact'], row['Contact Person'], row['Company Size']))
+            db.commit()
+        except Error as e:
+            db.rollback()
+            print(f"Error: '{e}'")
 
 
-
+user = 'root'
+password = getpass.getpass('Insert password for localhost --> ')
 try:
     print("Creating the database...\n")
     createdatabase(user=user, passw=password)
@@ -104,5 +148,6 @@ try:
     creattables(user=user, passw=password)
     print("Loading the dataset into the database...\n")
     dataload(user=user, password=password)
+    print("Database filled!")
 except Error as e:
     print(e)
