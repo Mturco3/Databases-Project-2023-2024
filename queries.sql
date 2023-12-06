850644b063c2ed1f46ea14693f3b47f23875cb5e
mtorella <marta.torella@studenti.luiss.it>
2023-12-06 20:05:22 +0100

    minor correction 1st sql query

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
 SELECT 
     role, 
     ROUND(AVG(CASE WHEN preference = 'Male' OR preference = 'Both' THEN max_salary END),1) AS male_salary,
-    ROUND(AVG(CASE WHEN preference = 'Male' OR preference = 'Both' THEN max_experience_years END),1) AS male_experience,
+    ROUND(AVG(CASE WHEN preference = 'Male' OR preference = 'Both' THEN min_experience_years END),1) AS male_experience,
     ROUND(AVG(CASE WHEN preference = 'Female' OR preference = 'Both' THEN max_salary END),1) AS female_salary,
-    ROUND(AVG(CASE WHEN preference = 'Female' OR preference = 'Both' THEN max_experience_years END),1) AS female_experience
+    ROUND(AVG(CASE WHEN preference = 'Female' OR preference = 'Both' THEN min_experience_years END),1) AS female_experience
 FROM Offer
 WHERE role = ':user_role' -- Specify a desired role
 GROUP BY role;
